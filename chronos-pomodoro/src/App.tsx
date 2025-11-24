503bd1c4ac477b604fa8795a30320f40bab91fb9
William Santana <williamandrade1058@gmail.com>
2025-11-24 11:47:11 -0300

    Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,24 +1,29 @@
-import { Heading } from "./components/Heading/Heading";
-import { TimerIcon } from "lucide-react";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello World!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
-        quisquam neque molestiae, deleniti dignissimos similique odio odit
-        aliquam officiis libero in quos at non illum sint velit magni soluta
-        fugiat?
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>Logo</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>Menu</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>Form</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>Footer</section>
+        </div>
+      </div>
     </>
   );
 }
