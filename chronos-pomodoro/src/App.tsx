1b26460232ed50af7aa71c2b56a2cc870dcee5e1
William Santana <williamandrade1058@gmail.com>
2025-11-24 15:09:00 -0300

    Criando o componente de logo com ícone e link

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,13 +1,15 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Heading } from "./components/Heading/Heading";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>Menu</Heading>
