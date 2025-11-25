e538eedd849a15d33a4c24ed432122617537bb55
William Santana <williamandrade1058@gmail.com>
2025-11-25 09:42:20 -0300

    Criando o componente de CountDown (Contador regressivo)

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -11,8 +12,13 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
