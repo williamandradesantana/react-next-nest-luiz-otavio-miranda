1de7e820678418688e5f41f5e5b80259fd622b6a
William Santana <williamandrade1058@gmail.com>
2025-11-27 15:02:44 -0300

    Capturando o evento de clique em link com preventDefault para tema dark e light

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -8,7 +6,6 @@ import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
 
 import { PlayCircleIcon } from "lucide-react";
 
@@ -16,33 +13,8 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  // const [count, setCount] = useState<number>(() => {
-  //   console.log("Lazy initilization");
-  //   return 0;
-  // });
-
-  const [count, setCount] = useState<number>(0);
-  const handleClick = () => {
-    // Pula de três em três por armazenar o estado anterior internamente
-    /*
-      setCount((prev) => prev + 1);
-      setCount((prev) => prev + 1);
-      setCount((prev) => prev + 1);
-    */
-
-    /* Executa apenas uma vez
-      setCount(count + 1);
-      setCount(count + 1);
-      setCount(count + 1);
-    */
-
-    setCount((prev) => prev + 1);
-  };
   return (
     <>
-      <Heading> {count} </Heading>
-      <button onClick={handleClick}>Aumentar</button>
-
       <Container>
         <Logo />
       </Container>
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,22 +1,56 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, type AnchorHTMLAttributes } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    console.log("clicado");
+  }
+
   return (
     <nav className={`${styles.menu}`}>
-      <a className={styles.menuLink} href="#">
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a home"
+        title="Ir para a home"
+      >
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver histórico"
+        title="Ver histórico"
+      >
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar tema"
+        title="Mudar tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
