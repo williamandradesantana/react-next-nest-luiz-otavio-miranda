406fcf7cf8d4bf6e60bf7ad4a19370fbce52c884
William Santana <williamandrade1058@gmail.com>
2025-11-28 16:58:57 -0300

    Salvando o tema no localStorage e trocando o ícone de sol e lua

--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,11 +1,28 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
+
 import styles from "./styles.module.css";
-import { useState } from "react";
 
 type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
@@ -14,14 +31,17 @@ export function Menu() {
 
     setTheme((prevTheme) => {
       const nextTheme = prevTheme === "dark" ? "light" : "dark";
-      document.documentElement.setAttribute("data-theme", nextTheme); // useEffect
       return nextTheme;
     });
   }
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className={`${styles.menu}`}>
-      <h1>{theme}</h1>
       <a
         className={styles.menuLink}
         href="#"
@@ -56,7 +76,7 @@ export function Menu() {
         title="Mudar tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
