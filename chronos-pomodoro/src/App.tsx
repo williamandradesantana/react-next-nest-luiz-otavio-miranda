2d2a2983baa33cf49f6ee7be57df38e6f4f74376
William Santana <williamandrade1058@gmail.com>
2025-12-02 17:57:14 -0300

    Estado global da aplicação com useState e comunicação via props

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 import { Home } from "./pages/Home";
+
+import type { TaskStateModel } from "./models/TaskStateModel";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsReaminig: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
--- chronos-pomodoro/src/pages/Home/index.tsx
+++ chronos-pomodoro/src/pages/Home/index.tsx
@@ -1,9 +1,15 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home({ state, setState }: HomeProps) {
   return (
     <MainTemplate>
       <Container>
