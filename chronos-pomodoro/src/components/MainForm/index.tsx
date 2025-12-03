d94bc8ce80c1f1b04bbd1595a7400ad52d5a2ace
William Santana <williamandrade1058@gmail.com>
2025-12-03 10:51:19 -0300

    Capturando o evento submit do formulário (onSubmit com preventDefault)

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -5,8 +5,13 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(1);
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <DefaultInput
           id="defaultInput"
