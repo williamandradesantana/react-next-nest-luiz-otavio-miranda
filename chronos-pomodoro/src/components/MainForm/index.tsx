d2e442384eb37fbee257efc6377633564bdeaaf2
William Santana <williamandrade1058@gmail.com>
2025-12-03 11:52:59 -0300

    Validando o formulário e criando uma nova tarefa

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -6,13 +6,54 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+
 export function MainForm() {
   // const [taskName, setTaskName] = useState<string>(""); utilizar quando precisar saber os valor em tempo real (cpf)
   const taskNameInput = useRef<HTMLInputElement>(null); // não causa várias renderizações no componente
 
+  const { setState } = useTaskContext();
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("Deu certo");
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: 1, // conferir
+        secondsRemaining: secondsRemaining, // conferir
+        formattedSecondsReaminig: "00:00", // conferir
+        tasks: [...prevState.tasks, newTask],
+        config: {
+          ...prevState.config,
+        },
+      };
+    });
+
+    taskNameInput.current.value = "";
   }
 
   return (
