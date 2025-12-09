dfc536a3085a000cc2ff19c9b0ba20218c3cabf4
William Santana <williamandrade1058@gmail.com>
2025-12-09 11:29:08 -0300

    Entendendo useReducer, reducer, dispatch e actions para trabalhar com o estado

--- chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -8,14 +8,37 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
+  const [number, dispatch] = useReducer((state, action) => {
+    console.log(action, state);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+    switch (action) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "RESET":
+        return 0;
+    }
+    return state;
+  }, 0);
+
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      {/* {children} */}
+      <h1>O número é: {number}</h1>
+      <button type="button" onClick={() => dispatch("INCREMENT")}>
+        Incrementar
+      </button>
+      <button type="button" onClick={() => dispatch("DECREMENT")}>
+        Decrementar
+      </button>
+      <button type="button" onClick={() => dispatch("RESET")}>
+        Resetar
+      </button>
     </TaskContext.Provider>
   );
 }
