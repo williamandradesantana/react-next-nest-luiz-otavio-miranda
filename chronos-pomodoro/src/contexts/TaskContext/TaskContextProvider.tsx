8d16f2d0742e74ed18855676c70d8bfd6210612c
William Santana <williamandrade1058@gmail.com>
2025-12-12 13:40:33 -0300

    web worker com setTimeout + useEffect para contagem regressiva

--- chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,10 +10,26 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((event) => {
+    const countDownSeconds = event.data;
+    console.log(event.data);
+
+    if (countDownSeconds <= 0) {
+      console.log("WORKER COMPLETED");
+      worker.terminate();
+    }
+  });
 
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      console.log("Worker fechou!");
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
--- chronos-pomodoro/src/workers/timerWorker.js
+++ chronos-pomodoro/src/workers/timerWorker.js
@@ -1,25 +1,25 @@
+let isRunning = false;
+
 self.onmessage = function (event) {
-  console.log("WORKER RECEBEU!", event.data);
+  if (isRunning) return;
 
-  switch (event.data) {
-    case "FAVOR": {
-      self.postMessage("Sim, posso fazer um favor!");
-      break;
-    }
+  isRunning = true;
 
-    case "FALA_OI": {
-      self.postMessage("OK, oi!");
-      break;
-    }
+  const state = event.data;
+  const { activeTask, secondsRemaining } = state;
 
-    case "FECHAR": {
-      self.postMessage("Tá bom");
-      self.close();
-      break;
-    }
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
 
-    default: {
-      self.postMessage("Não entendi!");
-    }
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+
+  function tick() {
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000);
+    setTimeout(tick, 1000);
   }
+
+  tick();
 };
