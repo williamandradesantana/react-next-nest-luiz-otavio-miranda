1ca348a38d728e11129a7684a5fd55c4ec5388d3
William Santana <williamandrade1058@gmail.com>
2025-12-19 14:50:45 -0300

    Formatando a data com date-fns

--- chronos-pomodoro/src/pages/History/index.tsx
+++ chronos-pomodoro/src/pages/History/index.tsx
@@ -6,6 +6,7 @@ import { MainTemplate } from "../../templates/MainTemplate";
 
 import styles from "./styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { formatDate } from "../../utils/formatDate";
 
 export function History() {
   const { state } = useTaskContext();
@@ -44,7 +45,7 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{formatDate(task.startDate)}</td>
                     <td>{task.interruptDate}</td>
                     <td>{task.type}</td>
                   </tr>
---
+++ chronos-pomodoro/src/utils/formatDate.ts
@@ -0,0 +1,7 @@
+import { format } from "date-fns";
+
+export function formatDate(timestamp: number) {
+  const date = new Date(timestamp);
+
+  return format(date, "dd/MM/yyyy HH:mm");
+}
