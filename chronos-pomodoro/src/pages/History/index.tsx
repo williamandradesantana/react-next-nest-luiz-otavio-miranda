8fdd192f72d918d5a5543abf1fd2903690889c44
William Santana <williamandrade1058@gmail.com>
2025-12-19 15:00:34 -0300

    Lógica do status da tarefa - Em progresso, Completa, Interrompida ou Abandonada

--- chronos-pomodoro/src/pages/History/index.tsx
+++ chronos-pomodoro/src/pages/History/index.tsx
@@ -7,6 +7,7 @@ import { MainTemplate } from "../../templates/MainTemplate";
 import styles from "./styles.module.css";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 export function History() {
   const { state } = useTaskContext();
@@ -46,7 +47,7 @@ export function History() {
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
                     <td>{task.type}</td>
                   </tr>
                 );
