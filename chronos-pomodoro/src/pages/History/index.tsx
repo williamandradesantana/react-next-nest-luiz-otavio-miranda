e1fa1a5450022aa82ec8d58355b072dccb76b6c2
William Santana <williamandrade1058@gmail.com>
2025-12-19 15:08:19 -0300

    Criando um dicionário para traduzir o type da tarefa

--- chronos-pomodoro/src/pages/History/index.tsx
+++ chronos-pomodoro/src/pages/History/index.tsx
@@ -42,13 +42,19 @@ export function History() {
             </thead>
             <tbody>
               {state.tasks.map((task) => {
+                const taskTypeDictionary = {
+                  workTime: "Foco",
+                  shortBreakTime: "Descanso curto",
+                  longBreakTime: "Descanso longo",
+                };
+
                 return (
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
                     <td>{formatDate(task.startDate)}</td>
                     <td>{getTaskStatus(task, state.activeTask)}</td>
-                    <td>{task.type}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
