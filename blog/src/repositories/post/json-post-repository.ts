f0c649bb2cd90c065a00b112e267db1c8420e30d
William Santana <williamandrade1058@gmail.com>
2026-01-08 23:50:15 -0300

    Padrão de projeto Repository para Posts - Parte 1 e 2

---
+++ blog/src/repositories/post/json-post-repository.ts
@@ -0,0 +1,36 @@
+import { PostModel } from "@/models/post/post-model";
+import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json"
+);
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+    if (!post) throw new Error("Post not found!");
+    return post;
+  }
+}
+
+export const postRepository: PostRepository = new JsonPostRepository();
