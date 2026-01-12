3481bd868f0e2d9b1e5caab9f35621909f9f5f5b
William Santana <williamandrade1058@gmail.com>
2026-01-12 10:33:48 -0300

    Container geral do site + tema claro e escuro no Tailwind CSS

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
+    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
       </header>
--- blog/src/repositories/post/json-post-repository.ts
+++ blog/src/repositories/post/json-post-repository.ts
@@ -12,7 +12,7 @@ const JSON_POSTS_FILE_PATH = resolve(
   "posts.json"
 );
 
-const SIMULATE_WAIT_IN_MS = 5000;
+const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements PostRepository {
   private async simulateWait() {
