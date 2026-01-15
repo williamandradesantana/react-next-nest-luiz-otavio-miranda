e01f373a3898635fe4c636c28b1c10ed1399a643
William Santana <williamandrade1058@gmail.com>
2026-01-15 15:03:48 -0300

    Header Component + Breakpoints do Tailwind CSS com mobile-first

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -6,9 +7,7 @@ import { Suspense } from "react";
 export default async function HomePage() {
   return (
     <Container>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é a header</h1>
-      </header>
+      <Header />
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
--- blog/src/components/Header/index.tsx
+++ blog/src/components/Header/index.tsx
@@ -1,23 +1,19 @@
-"use client";
-
 import clsx from "clsx";
+import Link from "next/link";
 
 export function Header() {
-  console.log("Header");
   return (
-    <h1
-      className={clsx(
-        "text-xl",
-        "font-bold",
-        "hover:text-blue-50",
-        "hover:bg-blue-500",
-        "transition",
-        "duration-300",
-        "text-blog"
-      )}
-      onClick={() => alert(123)}
-    >
-      Texto do meu h1
-    </h1>
+    <header>
+      <h1
+        className={clsx(
+          "text-4xl/normal font-extrabold py-8",
+          "sm:text-5xl/normal sm:py-10",
+          "md:text-6xl/normal md:py-11",
+          "lg:text-7xl/normal lg:py-12"
+        )}
+      >
+        <Link href="#">The Blog</Link>
+      </h1>
+    </header>
   );
 }
