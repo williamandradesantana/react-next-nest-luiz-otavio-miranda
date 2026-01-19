21b58694fa85fc6704298ade9495ea63f4b4c102
William Santana <williamandrade1058@gmail.com>
2026-01-19 10:02:30 -0300

    Componente FeaturedPost + Tailwind CSS Grid

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -2,12 +2,32 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+          ></Image>
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          corrupti sint iure inventore in culpa doloribus minus quos assumenda,
+          quod fuga consequuntur. Doloribus ut vitae sed mollitia qui enim
+          exercitationem.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
--- blog/src/components/Container/index.tsx
+++ blog/src/components/Container/index.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+    <div className="text-slate-900 bg-slate-100 min-h-screen">
       <div className="max-w-screen-lg mx-auto px-8">{children}</div>
     </div>
   );
