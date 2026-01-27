74e573553cd3daa2c9330a327bf65a8800fc0214
William Santana <williamandrade1058@gmail.com>
2026-01-27 18:42:51 -0300

    Criando a Grid responsiva de PostList com Tailwind CSS e nossos componentes

--- blog/src/components/PostHeading/index.tsx
+++ blog/src/components/PostHeading/index.tsx
@@ -9,15 +9,17 @@ type PostHeadingProps = {
 
 export function PostHeading({ children, url, as: Tag }: PostHeadingProps) {
   const headingClassesMap = {
-    h1: "text-2xl/tight sm:text-4xl",
-    h2: "text-2xl/tight sm:text-4xl",
+    h1: "text-2xl/tight sm:text-4xl font-extrabold",
+    h2: "text-2xl/tight font-bold",
   };
 
-  const commomClasses = "font-extrabold";
+  const commomClasses = "";
 
   return (
     <Tag className={clsx(headingClassesMap[Tag], commomClasses)}>
-      <Link href={url}>{children}</Link>
+      <Link className="group-hover:text-slate-600 transition" href={url}>
+        {children}
+      </Link>
     </Tag>
   );
 }
--- blog/src/components/PostsList/index.tsx
+++ blog/src/components/PostsList/index.tsx
@@ -1,13 +1,42 @@
 import { PostModel } from "@/models/post/post-model";
 import { postRepository } from "@/repositories/post";
+import { PostCoverImage } from "../PostCoverImage";
+import { PostHeading } from "../PostHeading";
 
 export async function PostsList() {
   const posts: PostModel[] = await postRepository.findAll();
 
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div className="flex flex-col group gap-4" key={post.id}>
+            <PostCoverImage
+              linkProps={{ href: `/post/${post.slug}` }}
+              imageProps={{
+                width: 1200,
+                height: 720,
+                src: post.coverImageUrl,
+                alt: post.title,
+              }}
+            />
+
+            <div className="flex flex-col gap-4 sm:justify-center">
+              <time
+                className="text-slate-600 text-sm/tight"
+                dateTime={post.createdAt}
+              >
+                {post.createdAt}
+              </time>
+
+              <PostHeading url="#" as="h2">
+                {post.title}
+              </PostHeading>
+
+              <p>{post.excerpt}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
