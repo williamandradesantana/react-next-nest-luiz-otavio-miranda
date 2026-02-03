f48a3e5466818b3673908d2d68e716528ab8c798
William Santana <williamandrade1058@gmail.com>
2026-02-03 09:30:15 -0300

    Criando rotas dinâmicas no Next.js

--- blog/src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About",
-};
-
-export default function AboutPage() {
-  return <h1>About</h1>;
-}
---
+++ blog/src/app/post/[slug]/page.tsx
@@ -0,0 +1,10 @@
+type PostSlugPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
+  return (
+    <h1 className="text-7xl font-extrabold py-16">Rota dinâmica: {slug}</h1>
+  );
+}
