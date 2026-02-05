3d38aaa12d994836a585ee00e15fa6e3b0c32289
William Santana <williamandrade1058@gmail.com>
2026-02-05 10:33:11 -0300

    generateMetadata - Gerando title, description ou outras metas dinâmicamente

--- blog/src/app/post/[slug]/page.tsx
+++ blog/src/app/post/[slug]/page.tsx
@@ -1,8 +1,21 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
