500785ac99780622dbd3883a02d448b851797fb2
William Santana <williamandrade1058@gmail.com>
2026-02-10 12:26:44 -0300

    Configurando o plugin Tailwind Typography do Tailwind CSS

--- blog/src/components/SafeMarkdown/index.tsx
+++ blog/src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import ReactMarkdown from "react-markdown";
 import rehypeSanitize from "rehype-sanitize";
 import remarkGfm from "remark-gfm";
@@ -8,7 +9,20 @@ type SafeMarkdownProps = {
 
 export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
+    <div
+      className={clsx(
+        "prose prose-slate",
+        "w-full max-w-none",
+        "overflow-hidden",
+        "prose-a:transition",
+        "prose-a:no-underline",
+        "prose-a:text-blue-500",
+        "prose-a:hover:text-blue-700",
+        "prose-a:hover:underline",
+        "prose-img:mx-auto",
+        "lg:prose-lg",
+      )}
+    >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
