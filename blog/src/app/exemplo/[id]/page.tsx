6d2cb5dae9becb82c15573d7b85176a17063ff5e
William Santana <williamandrade1058@gmail.com>
2026-03-04 14:14:12 -0300

    Full Route Cache com dynamic e dynamicParams ou generateStaticParams

--- blog/src/app/exemplo/[id]/page.tsx
+++ blog/src/app/exemplo/[id]/page.tsx
@@ -1,11 +1,26 @@
 import { formatHour } from "@/utils/format-datetime";
 
-export default async function ExemploIdPage() {
+// export const dynamic = "force-static";
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }];
+}
+
+export default async function ExemploIdPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const hour = formatHour(Date.now());
 
   return (
     <main className="min-h-[600px] text-xl font-bold">
-      <div> {hour} </div>
+      <div>
+        {" "}
+        {hour} - id {id}
+      </div>
     </main>
   );
 }
--- blog/src/app/exemplo/page.tsx
+++ blog/src/app/exemplo/page.tsx
@@ -1,5 +1,7 @@
 import { formatHour } from "@/utils/format-datetime";
 
+export const dynamic = "force-dynamic";
+
 export default async function ExemploPage() {
   const hour = formatHour(Date.now());
 
