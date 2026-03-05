f3acff86f9be0648eca47a282feec422589f37fa
William Santana <williamandrade1058@gmail.com>
2026-03-05 13:00:26 -0300

    Usando revalidatePath para revalidar o cache manualmente

--- blog/src/app/actions/revalidate-example.ts
+++ blog/src/app/actions/revalidate-example.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function revalidateExampleAction(formData: FormData) {
-  const path = formData.get("path") || "";
+  const path = (formData.get("path") as string) || "";
 
   console.log(`Estou em uma server action - ${path}`);
+
+  revalidatePath(`${path}`);
 }
--- blog/src/app/exemplo/[id]/page.tsx
+++ blog/src/app/exemplo/[id]/page.tsx
@@ -2,7 +2,7 @@ import { revalidateExampleAction } from "@/app/actions/revalidate-example";
 import { formatHour } from "@/utils/format-datetime";
 
 export const dynamic = "force-static";
-// export const revalidate = 10;
+export const revalidate = 30;
 
 export default async function ExemploIdPage({
   params,
