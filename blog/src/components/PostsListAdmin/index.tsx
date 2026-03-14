e0cc873c82e8cd4faf3e4d072a8a5d4e37b00e85
William Santana <williamandrade1058@gmail.com>
2026-03-14 11:03:54 -0300

    Estilizando PostsListAdmin com Tailwind CSS

--- blog/src/components/PostsListAdmin/index.tsx
+++ blog/src/components/PostsListAdmin/index.tsx
@@ -1,15 +1,44 @@
 import { findAllPostsAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import Link from "next/link";
+import { Trash2Icon } from "lucide-react";
 
 export async function PostsListAdmin() {
   const posts = await findAllPostsAdmin();
 
   return (
-    <div className="py-16">
-      <div>
-        {posts.map((post) => {
-          return <p key={post.id}>{post.title}</p>;
-        })}
-      </div>
+    <div className="mb-16">
+      {posts.map((post) => {
+        return (
+          <div
+            className={clsx(
+              "py-2 px-2",
+              !post.published && "bg-slate-300",
+              "flex gap-2 items-center justify-between",
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+            {!post.published && (
+              <span className="text-xs text-slate-600 italic">
+                (Não publicado)
+              </span>
+            )}
+
+            <button
+              className={clsx(
+                "text-red-500 cursor-pointer transition",
+                "[&_svg]:w-4 [&_svg]:h-4",
+                "hover:scale-120 hover:text-red-700",
+              )}
+              aria-label={`Apagar post: ${post.title}`}
+              title={`Apagar post: ${post.title}`}
+            >
+              <Trash2Icon />
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
