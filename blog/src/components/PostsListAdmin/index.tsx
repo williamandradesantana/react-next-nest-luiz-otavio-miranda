9d70c3c975dedf283d25468d65f4b715da1548f2
William Santana <williamandrade1058@gmail.com>
2026-03-18 13:54:18 -0300

    Layout de um componente de caixa de diálogo com Tailwind CSS (Dialog)

--- blog/src/components/PostsListAdmin/index.tsx
+++ blog/src/components/PostsListAdmin/index.tsx
@@ -29,6 +29,51 @@ export async function PostsListAdmin() {
           </div>
         );
       })}
+
+      <div
+        className={clsx(
+          "fixed z-50 inset-0 bg-black/50 backdrop-blur-xs",
+          "flex items-center justify-center",
+        )}
+      >
+        <div
+          className={clsx(
+            "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6",
+            " flex flex-col gap-6",
+            "shadow-lg shadow-black/30 text-center",
+          )}
+        >
+          <h3 className="text-xl font-extrabold">Título do diálogo</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+            necessitatibus quasi dolorem accusamus inventore et fuga, fugiat
+            voluptatum autem delectus rerum quod esse recusandae corrupti
+            voluptatem, odio, a deserunt eligendi?
+          </p>
+
+          <div className="flex items-center justify-around">
+            <button
+              className={clsx(
+                "bg-slate-300 hover:bg-slate-400 transition text-slate-950",
+                "flex items-center justify-center",
+                "py-2 px-4 cursor-pointer rounded-lg",
+              )}
+              autoFocus
+            >
+              Cancelar
+            </button>
+            <button
+              className={clsx(
+                "bg-blue-500 hover:bg-blue-600 transition text-blue-50",
+                "flex items-center justify-center",
+                "py-2 px-4 cursor-pointer rounded-lg",
+              )}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
