53b6aa114f6e83fb472a8e5a01fe24860a751244
William Santana <williamandrade1058@gmail.com>
2026-03-19 14:37:52 -0300

    Estilizando o MenuAdmin com Tailwind CSS

--- blog/src/components/Admin/MenuAdmin/index.tsx
+++ blog/src/components/Admin/MenuAdmin/index.tsx
@@ -1,13 +1,33 @@
+import clsx from "clsx";
+import { FileTextIcon, HouseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    "bg-slate-900 text-slate-100 rounded-lg",
+    "flex flex-col mb-8",
+    "sm:flex-row sm:flex-wrap",
+    //"h-10 overflow-hidden",
+  );
+  const linkClasses = clsx(
+    "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
+    "flex items-center justify-start gap-2",
+    "transition hover:bg-slate-800 rounded-lg",
+    "h-10",
+    "shrink-0",
+  );
+
   return (
-    <nav>
-      <a href="/" target="_blank">
+    <nav className={navClasses}>
+      <a href="/" className={linkClasses} target="_blank">
+        <HouseIcon />
         Home
       </a>
 
-      <Link href="/admin/post">Posts</Link>
+      <Link href="/admin/post" className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
