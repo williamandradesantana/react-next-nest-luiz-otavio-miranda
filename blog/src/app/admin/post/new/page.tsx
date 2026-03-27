63f6402f77404ce573e8644281d81b46aaa2b792
William Santana <williamandrade1058@gmail.com>
2026-03-27 15:53:27 -0300

    Formulário de criação e edição do post (ManagePostForm)

--- blog/src/app/admin/post/new/page.tsx
+++ blog/src/app/admin/post/new/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/Button";
-import { InputCheckbox } from "@/components/InputCheckbox";
-import { InputText } from "@/components/InputText";
+import { ManagePostForm } from "@/components/Admin/ManagePostForm";
 import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
@@ -10,21 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default async function AdminPostNewPage() {
-  return (
-    <form action="" className="mb-16">
-      <div className="flex flex-col gap-6">
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
-        <InputText labelText="Email" placeholder="Digite seu email" />
-        <InputCheckbox labelText="Sobrenome" type="checkbox" />
-        <InputText labelText="Sobrenome" defaultValue="Olá mundo" disabled />
-        <InputText labelText="Sobrenome" defaultValue="Olá mundo" readOnly />
-      </div>
-
-      <div className="mt-4">
-        <Button type="submit" variant="default" size="md">
-          Enviar
-        </Button>
-      </div>
-    </form>
-  );
+  return <ManagePostForm />;
 }
---
+++ blog/src/components/Admin/ManagePostForm/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { InputCheckbox } from "@/components/InputCheckbox";
+import { InputText } from "@/components/InputText";
+
+export function ManagePostForm() {
+  return (
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText labelText="Nome" placeholder="Digite seu nome" />
+        <InputText labelText="Email" placeholder="Digite seu email" />
+        <InputCheckbox labelText="Sobrenome" type="checkbox" />
+        <InputText labelText="Sobrenome" defaultValue="Olá mundo" disabled />
+        <InputText labelText="Sobrenome" defaultValue="Olá mundo" readOnly />
+      </div>
+
+      <div className="mt-4">
+        <Button type="submit" variant="default" size="md">
+          Enviar
+        </Button>
+      </div>
+    </form>
+  );
+}
