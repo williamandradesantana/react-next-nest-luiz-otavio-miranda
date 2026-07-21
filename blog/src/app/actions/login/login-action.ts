f44217877f0e8f34ca0c2344460929c0f6b69822
William Santana <williamandrade1058@gmail.com>
2026-07-21 11:01:50 -0300

    Trabalhando com cookies seguros no Next.js

--- blog/src/app/actions/login/login-action.ts
+++ blog/src/app/actions/login/login-action.ts
@@ -1,7 +1,8 @@
 "use server";
 
-import { verifyPassword } from "@/lib/login/manage-login";
+import { createLoginSession, verifyPassword } from "@/lib/login/manage-login";
 import { asyncDelay } from "@/utils/async-delay";
+import { redirect } from "next/navigation";
 
 type LoginActionState = {
   username: string;
@@ -42,9 +43,6 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
   }
 
   // cria o cookie e redirencionar a página
-
-  return {
-    username: username,
-    error: "Usuário válido com sucesso",
-  };
+  await createLoginSession(username);
+  redirect("/admin/post");
 }
--- blog/src/lib/login/manage-login.ts
+++ blog/src/lib/login/manage-login.ts
@@ -1,4 +1,12 @@
 import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
+const jwtEncodedKey = new TextEncoder().encode(jwtSecretKey);
+
+const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
+const loginExpStr = process.env.LOGIN_EXPIRATION_STRING || "1d";
+const loginCookieName = process.env.LOGIN_COOKIE_NAME || "loginSession";
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10);
@@ -9,4 +17,23 @@ export async function hashPassword(password: string) {
 export async function verifyPassword(password: string, base64Hash: string) {
   const hash = Buffer.from(base64Hash, "base64").toString("utf-8");
   return bcrypt.compare(password, hash);
+}
+
+export async function createLoginSession(username: string) {
+  const expiresAt = new Date(Date.now() + loginExpSeconds * 1000);
+  const loginSession = username + " Qualquer coisa";
+  const cookieStore = await cookies();
+
+  cookieStore.set(loginCookieName, loginSession, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    expires: expiresAt,
+  });
+}
+
+export async function deleteLoginSession() {
+  const cookieStore = await cookies();
+  cookieStore.set(loginCookieName, "", { expires: new Date(0) });
+  cookieStore.delete(loginCookieName);
 }
